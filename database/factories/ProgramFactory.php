8e08f31a6e0c275b95224c4ae6273dddb790a7f3
Joseph Vanden Bos <joseph.vandenbos@rocky.edu>
2020-10-29 10:03:23 -0600

    added table definitions

---
+++ database/factories/ProgramFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\program;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class programFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = program::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
