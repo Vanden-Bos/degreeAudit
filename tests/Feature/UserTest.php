d4b3a397cf7e53b632190c89682ba6d211a03055
Joseph Vanden Bos <joseph.vandenbos@rocky.edu>
2020-11-02 21:47:40 -0700

    I'm 90% sure I fixed the stupidity that is deleting test files that I falsely assumed were unnecessary.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -16,15 +16,7 @@ class User extends TestCase
      */
     public function testExample()
     {
-        // https://laravel.com/docs/8.x/database-testing
-        // $user = factory(App/Models/User)->create(['first_name' => 'testName', 'last_name' => 'testLastName', 
-        //     'email' => 'test@example.com', 'email_verified_at' => now(), 'password' => 'password']);
-        // $this->assertDatabaseHas('users', ['first_name' => 'testName', 'last_name' => 'testLastName',
-        //     'email' => 'test@example.com', 'email_verified_at' => now(), 'password' => 'password']);
-
-        $user = \App\Models\User::create  (['first_name' => 'John', 'last_name' => 'Doe', 
-            'email' => 'johndoe@example.com', 'email_verified_at' => now(), 'password' => 'password']);
-        $this->assertDatabaseHas ('users', ['first_name' => 'John', 'last_name' => 'Doe', 
-            'email' => 'johndoe@example.com', 'email_verified_at' => now(), 'password' => 'password']);
+        $user = \App\Models\User::create  (['first_name' => 'John', 'last_name' => 'Doe', 'email' => 'johndoe@example.com', 'email_verified_at' => now(), 'password' => 'password']);
+        $this->assertDatabaseHas ('users', ['first_name' => 'John', 'last_name' => 'Doe', 'email' => 'johndoe@example.com', 'email_verified_at' => now(), 'password' => 'password']);
     }
 }
